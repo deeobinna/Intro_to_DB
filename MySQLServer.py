240f3d069f5cb0640ca7ef361279c5a3fc790b0f
DesmondObinna <talk2desmond91@gmail.com>
2025-12-14 12:25:24 +0100

    MySQLServer

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,8 +17,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully.")
     
-    except Error as e:
-        print(f"Error: {e}")
+    except mysql.connector.Error as e:
+        print(f"Error: Failed to connect to the database or create database. {e}")
     
     finally:
         if "connection" in locals() and connection.is_connected():
