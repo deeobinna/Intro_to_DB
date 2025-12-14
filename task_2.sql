720ab1baa862488618586bebaf89c00e52462fb1
DesmondObinna <talk2desmond91@gmail.com>
2025-12-14 12:39:02 +0100

    TASK2

--- task_2.sql
+++ task_2.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details(
@@ -33,6 +33,6 @@ CREATE TABLE IF NOT EXISTS Order_Details(
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 )
