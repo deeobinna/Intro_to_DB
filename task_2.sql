6b59bffb2e252a2f1d7ec228d3d6d95985a394c9
DesmondObinna <talk2desmond91@gmail.com>
2025-12-14 12:36:14 +0100

    TASK2

--- task_2.sql
+++ task_2.sql
@@ -1,6 +1,6 @@
 USE alx_book_store:
 
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL
 );
