4164a55357981248f0edce9b5563749470676c94
DesmondObinna <talk2desmond91@gmail.com>
2025-12-14 12:04:32 +0100

    SQL Table

---
+++ alx_book_store.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id foreign key REFERENCES Authors(author_id),
+    publication_date DATE,
+    price DOUBLE NOT NULL
+);    
+
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL
+    address TEXT NOT NULL
+);
+
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id foreign key REFERENCES Customers(customer_id),
+    order_date DATE NOT NULL,
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id foreign key REFERENCES Orders(order_id),
+    book_id foreign key REFERENCES Books(book_id),
+    quantity DOUBLE NOT NULL
+)
